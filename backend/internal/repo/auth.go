57966714dbad34faf8c0cdd952f87e91e25223ae
PluxuryPascal <miakulov@edu.hse.ru>
2026-02-26 01:53:32 +0500

    feat: Implement database interaction for user login and registration using `pgx`.

--- backend/internal/repo/auth.go
+++ backend/internal/repo/auth.go
@@ -5,6 +5,9 @@ import (
 	"backend/internal/domain"
 	"context"
 	"errors"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
 )
 
 type UserRepository interface {
@@ -23,16 +26,55 @@ func NewUserRepo(dbClient *db.PostgresClient) UserRepository {
 }
 
 func (r *userRepo) Login(ctx context.Context, login string) (*domain.UserLogin, error) {
-	return &domain.UserLogin{
-		ID:         "mock-id-123",
-		Password:   "$argon2id$v=19$m=65536,t=1,p=4$4wDt2Cx/ryTr0/THdujebA$hCLlQYObN74RQSKB9ZXImegzFOV+UdWdHvi+9iDiV0g", // argon2 hash of 'password123'
-		GroupAlias: "admin",
-	}, nil
+	query := `
+		SELECT
+			u.id AS user_id,
+			u.password_hash AS password,
+			t.name AS goup_alias
+		FROM auth.t_users u
+			JOIN auth.t_teams t on t.id = u.team_id
+		WHERE u.email = @login;
+	`
+
+	result, err := r.dbClient.Pool.Query(ctx, query, pgx.NamedArgs{
+		"login": login,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("exec error: %w", err)
+	}
+
+	user, err := pgx.RowToStructByName[domain.UserLogin](result)
+	if err != nil {
+		return nil, fmt.Errorf("scan row error: %w", err)
+	}
+
+	return &user, nil
 }
 
 func (r *userRepo) Register(ctx context.Context, user *domain.UserRegister) (*domain.User, error) {
-	return &domain.User{
-		ID:         "mock-id-123",
-		GroupAlias: "admin",
-	}, nil
+	query := `
+		INSERT INTO auth.t.users u (
+			email,
+			password_hash
+		)
+		VALUES (
+			@email,
+			@password_hash
+		)
+	`
+
+	result, err := r.dbClient.Pool.Query(ctx, query, pgx.NamedArgs{
+		"email":         user.Mail,
+		"password_hash": user.Password,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("exec error: %w", err)
+	}
+
+	createdUser, err := pgx.RowToStructByName[domain.User](result)
+	if err != nil {
+		return nil, fmt.Errorf("scan row error: %w", err)
+	}
+
+	return &createdUser, nil
 }
